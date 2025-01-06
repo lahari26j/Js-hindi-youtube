a3d390fc0c77a9a80b0ced788a03c71b1760ad09
Jamalpuri Lahari <141894151+lahari26j@users.noreply.github.com>
2025-01-06 07:58:55 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all  js code as newer version
+
+//alert( 3+3) //we are using nodejs, not browser
+
+console.log(3 + 3); // code  readability should be high
+
+console.log("Hitesh")
+
+let name= "Hitesh"
+let age=18
+let isLoggedIn = false
+
+// -------PRIMITIVE DATATYPES----------------
+// number => 2 to power 53
+// bigint => for large numbers prefer this
+// string =>prefer("")
+// boolean => true/false
+// null => standalone value(Empty--i.e let state =null)
+// unefined =>not defined value
+// symbol => unique
+
+// OBJECT
+
+console.log(typeof null); // object
+console.log((typeof undefined)); // undefined
