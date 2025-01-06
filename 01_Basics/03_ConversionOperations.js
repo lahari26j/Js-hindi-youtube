520b4e5fc4ba8b7e6763ba3e3fcf760580ac5954
Jamalpuri Lahari <141894151+lahari26j@users.noreply.github.com>
2025-01-06 08:27:46 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,26 @@
+let score = "Hitesh"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = "Hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 0 => false 1 =>true
+// "" => false
+//"hitesh" =>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
