7a8106e2947a292c172c011c26480fef81e1c846
Jamalpuri Lahari <141894151+lahari26j@users.noreply.github.com>
2025-01-06 09:34:38 +0000

    Conversation of Datatypes

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,10 +1,10 @@
 let score = "Hitesh"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33" =>33
 // "33abc" => NaN
@@ -12,15 +12,63 @@ console.log(valueInNumber);
 let isLoggedIn = "Hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 0 => false 1 =>true
 // "" => false
 //"hitesh" =>true
 
 let someNumber = 33
+
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+//console.log(stringNumber);
+//console.log( typeof stringNumber);
+
+// ************************ Operations  *************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Hitesh"
+let str3 = str1 + str2
+//console.log(str3); //hellohitesh
+
+//console.log("1"+2); //12
+//console.log(1+"2"); //12
+//console.log("1"+2+2); //122
+//console.log(1+2+"3"); //33
+// folllows 1st type will be code as that type o/p
+
+//console.log(+true); //1
+//console.log(+" ");
+// should not use so far like this 
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
 
