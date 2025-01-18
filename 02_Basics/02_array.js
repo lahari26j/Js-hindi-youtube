4e67009353ceaf8734433122cc4a34dee2717bf4
Jamalpuri Lahari <141894151+lahari26j@users.noreply.github.com>
2025-01-18 15:30:15 +0000

    arrays methodds

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -15,7 +15,7 @@ const all_new_heros = [...k_heros, ...vampires]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+//console.log(real_another_array);
 
 
 
